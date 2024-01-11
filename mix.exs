a54999fae912e9eec1158acdd41b680546aa10f6
kagure-nyakio <kagurenyakio@gmail.com>
2024-01-11 19:44:59 +0300

    Generate phoenix auth files

--- mix.exs
+++ mix.exs
@@ -53,6 +53,7 @@ defmodule InvoiceGenerator.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:bcrypt_elixir, "~> 3.0"},
       {:phoenix, "~> 1.7.10"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.10"},
